6e14db7b7d6f0192088377190b33108a242640d7
Akib <ali.haider@brainstation-23.com>
2024-10-18 11:57:24 +0600

    Implemented live tracking of active users using SignalR connection count.

--- SignalRSample/wwwroot/js/userCount.js
+++ SignalRSample/wwwroot/js/userCount.js
@@ -8,13 +8,17 @@ connectionUserCount.on("updateTotalViews", (value) => {
     newCountSpan.innerText = value.toString();
 });
 
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
 //invoke hub methods aka send notification to hub
 function newWindowLoadedOnClient() {
     connectionUserCount.send("NewWindowLoaded");
 }
 
 //start connection
-
 function fulfilled() {
     //do something on start
     console.log("Connection to User Hub Successful!");
